b1836d65d56334cda9cc6f3f7834af7f88226dab
Anton Marin <antonaryo85@gmail.com>
2023-10-18 21:31:13 +0300

    generate plantUML description from database schema

---
+++ idea/extensions/database-sql/schema/Generate PlantUML.groovy
@@ -0,0 +1,50 @@
+import com.intellij.database.model.DasColumn
+import com.intellij.database.model.DasIndex
+import com.intellij.database.model.DasTable
+import com.intellij.database.util.Case
+import com.intellij.database.util.DasUtil
+
+/*
+ * Available context bindings:
+ *   SELECTION   Iterable<DasObject>
+ *   PROJECT     project
+ *   FILES       files helper
+ */
+FILES.chooseDirectoryAndSave("Choose directory", "Choose where to store UML file") { dir ->
+    generate(SELECTION.filter { it instanceof DasTable }, dir)
+}
+
+static def generate(tables, dir) {
+    new File(dir, "output.plantuml")
+            .withPrintWriter { out ->
+                out.println "@startuml"
+                out.println "top to bottom direction"
+                tables.each { generateTable(out, it) }
+                out.println "@enduml"
+            }
+}
+
+static def generateTable(out, DasTable table) {
+    def tableCommentExpr = table.getComment() ? " as \"${table.getName()} /* ${table.getComment()} */\"" : ""
+    out.println "class ${table.getName()}$tableCommentExpr {"
+    DasUtil.getColumns(table).each {
+        generateColumn(out, it)
+    }
+    DasUtil.getIndices(table).each {
+        generateIndex(out, it)
+    }
+    out.println "}"
+}
+
+static def generateColumn(out, DasColumn col) {
+    def columnCommentExpr = col.getComment() ? " /* ${col.getComment()} */" : ""
+    def columnNullExp = col.isNotNull() ? " not null" : ""
+    def pkExpr = DasUtil.isPrimary(col) ? " primary key" : ""
+    out.println "\t${col.getName()}$columnCommentExpr: ${col.getDasType().getSpecification()}${columnNullExp}${pkExpr}"
+}
+static def generateIndex(out, DasIndex ind) {
+    def indCommentExpr = ind.getComment() ? " /* ${ind.getComment()} */" : ""
+    def indColumns = ind.columnsRef.names().join(',')
+    def indUniqueExpr = ind.unique ? " unique" : ""
+    out.println "\t${ind.getName()}($indColumns)${indCommentExpr}${indUniqueExpr}"
+}
---
+++ idea/extensions/database-sql/schema/README.md
@@ -0,0 +1,4 @@
+# Database schemas extensions
+
+Place to IDEA -> Project/Scratches and Consoles/Extensions/Database Tools and SQL/schema to support apply script
+to database schemas
